babdb7ee54c4e5a4d68b96da546773874003e080
ZannaC <zhanna_s1@inbox.lv>
2023-06-27 14:50:34 +0300

    Add Footer section

---
+++ spark-proj/src/components/FooterBlocks/AboutBlock.js
@@ -0,0 +1,22 @@
+function AboutBlock() {
+  return (
+    <div>
+      <ul>
+        <li className="li-title">About </li>
+        <li>
+          <a href="#top">Our Story</a>
+        </li>
+        <li>
+          <a href="#top">Benefits</a>
+        </li>
+        <li>
+          <a href="#top">Team</a>
+        </li>
+        <li>
+          <a href="#top">Careers</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default AboutBlock;
