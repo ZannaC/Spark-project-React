06b755fccfb6641dd1ed9729c95165a65399314f
ZannaC <zhanna_s1@inbox.lv>
2023-07-11 09:53:53 +0300

    Add search input changes using useState

--- spark-proj/src/components/DropDownMenu/SearchBlock.js
+++ spark-proj/src/components/DropDownMenu/SearchBlock.js
@@ -1,19 +1,36 @@
+import React, { useState } from "react";
 import search from "../../assets/images/dropDownMenu/search.svg";
 import DropDown from "./DropDown";
 import "./dropDownMenu.css";
 
 const SearchBlock = (props) => {
+  const [searchData, setSearchData] = useState({ search: "" });
+
+  const onChangeSearch = (event) => {
+    setSearchData((currentData) => ({
+      ...currentData,
+      search: event.target.value,
+    }));
+  };
+
   return (
     <header className="drop-down-menu-container">
       <button type="button" onClick={props.onCancel} className="close-btn">
         ✖
       </button>
-      <div className="search-close-block">
-        <button className="search-btn">
-          <a href="top#">
-            <img src={search} alt="search icon" />
+      <div className="search-block">
+        <div className="search-input-wrapper">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Type here to search"
+            value={searchData.search}
+            onChange={onChangeSearch}
+          />
+          <a href="https://www.w3schools.com" target="blank">
+            <img src={search} alt="search icon" className="search-icon" />
           </a>
-        </button>
+        </div>
       </div>
 
       <DropDown />
